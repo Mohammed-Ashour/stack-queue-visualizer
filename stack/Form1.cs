967b1f9f6cbf98d7fe2391f885f44e8fe486b95d
Mohammed Ashour <m.aly.ashour@gmail.com>
2015-03-30 18:14:41 +0200

    first update

--- stack/Form1.cs
+++ stack/Form1.cs
@@ -50,7 +50,9 @@ namespace stack
             stack_s = stack_s +Environment.NewLine + textBox1.Text;
             //option to clear the elements textbox
             //textBox1.Text = "";
-            textBox4.Text = array_stack.First() + Environment.NewLine + textBox4 .Text ; 
+            textBox6.Text = array_stack.First() + Environment.NewLine + textBox6 .Text ;
+            label8.Text = my_stack.Peek().ToString();
+
 
         }
 
@@ -80,25 +82,29 @@ namespace stack
 
                 string pop_val = my_stack.Pop().ToString();
                 stack_s = my_stack.ToString();
-                textBox4.Text = "";
-                for (int i = 0; i < array_stack.Length; i++)
-                {
-                    if (array_stack[i].ToString() == pop_val)
-                    {
-                        string rm = array_stack[i].ToString();
-                        array_stack = array_stack.Where(val => val != rm).ToArray();
-                    }
-
-
-                }
+                textBox6.Text = "";
+               
+                string rm = array_stack[0].ToString();
+                array_stack = array_stack.Where(val => val != rm).ToArray();
+                    
                 //rebuild our textbox after modification
 
                 for (int i = array_stack.Length - 1; i >= 0; i--)
                 {
-                    textBox4.Text = array_stack[i] + Environment.NewLine + textBox4.Text;
+                    textBox6.Text = array_stack[i] + Environment.NewLine + textBox6.Text;
 
                 }
+                if (array_stack.Length > 0)
+                {
+
+                    label8.Text = my_stack.Peek().ToString();
+                }
+                else
+                {
+                    label8.Text = "None";
+                }
             }
+
 
         }
 
@@ -111,8 +117,8 @@ namespace stack
             //textBox3.Text =queue_s;
             //option to clear the elements textbox
             //textBox1.Text = "";
-            textBox3.Text += array_queue.Last() + Environment.NewLine ; 
-
+            textBox3.Text += array_queue.Last() + Environment.NewLine ;
+            label9.Text = my_queue.Peek().ToString();
 
         }
 
@@ -129,24 +135,40 @@ namespace stack
                 string pop_val = my_queue.Dequeue().ToString();
                 queue_s = my_queue.ToString();
                 textBox3.Text = "";
-                for (int i = 0; i < array_queue.Length; i++)
-                {
-                    if (array_queue[i].ToString() == pop_val)
-                    {
-                        string rm = array_queue[i].ToString();
-                        array_queue = array_queue.Where(val => val != rm).ToArray();
-                    }
-
-
-                }
+                //removing the poped item from our array
+                string rm = array_queue[0].ToString();
+                array_queue = array_queue.Where(val => val != rm).ToArray();
+                    
                 //rebuild our textbox after modification
                 for (int i = array_queue.Length - 1; i >= 0; i--)
                 {
                     textBox3.Text = array_queue[i] + Environment.NewLine + textBox3.Text;
 
                 }
-            }
+                if (array_queue.Length > 0)
+                {
 
+                    label9.Text = my_queue.Peek().ToString();
+                }
+                else
+                {
+                    label9.Text = "None";
+                }
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
 
         }
     }
